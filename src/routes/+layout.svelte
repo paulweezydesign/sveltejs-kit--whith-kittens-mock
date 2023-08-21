51f57587737963abca4edf9d3e39ee574d1a42b9
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2023-08-21 10:42:43 +0000

    4 files modified, 1 added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+		import { SvelteUIProvider } from '@svelteuidev/core';
 </script>
-
+<SvelteUIProvider  withGlobalStyles themeObserver={'dark'}>
 <div class="app">
 	<Header />
 
@@ -14,40 +15,10 @@
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
-
+</SvelteUIProvider >
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+	.dark-theme {
+		background: black;
+		color: white;
 	}
 </style>
